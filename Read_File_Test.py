70288b7d8f4ea9c59bf535622266c2715b25dbb4
Esther Schenau <estherschenau@utexas.edu>
2014-10-15 16:33:01 -0500

    Working IO

--- Read_File_Test.py
+++ Read_File_Test.py
@@ -1,31 +1,49 @@
+#!/usr/bin/env/python3
 #Esther Schenau, Cameron Miller
-
+class Ballot(object):
+	def __init__(self, ballot): 
+		self.ballot = tuple(ballot)
+	
+	
 
 def ReadFile (r, w): 
+	
+	#-----Collection of Lists-------
+
 	list_lines = []
-	candidates = []
+	list_candidates = []
+	list_ballots = []
+
+	#-------------------------------
+	
 	for lines in r: 
 		lines = lines.strip() 
 		list_lines.append(lines)
-
-	number_of_elections = list_lines[0]
+	number_of_elections = int(list_lines[0])
 	list_lines.pop(0)
-
-	print (number_of_elections)
-	print (list_lines)
 
 	while len(list_lines) > 0:
 		#LOI = line of interest
 		for i in list_lines:
 			loi = list_lines[0]
 			if loi == "": 
-				number_candidates = int(list_lines [1])
-				print ("Number cand:", number_candidates)
 				list_lines.pop(0)
-				for i in range (1, number_candidates+1):
-					candidates.append(list_lines[i]) 
+				number_candidates = int(list_lines [0])
+				list_lines.pop(0)
+
+				for i in range (0, number_candidates):
+					list_candidates.append(list_lines[0]) 
+					list_lines.pop(0)
+
 			else: 
 				#READ BALLOTS IN HERE
+				#make list a Ballot object
+				temp = loi.split()
+				temp = [int (c) for t in temp for c in t]
+				list_ballots.append(temp)
 				list_lines.pop(0)
-	print (candidates)
-	print ("list lines:", list_lines)
+	
+
+	print ("number of elections:", number_of_elections)
+	print ("Candidates:",list_candidates)
+	print ("Ballots:", list_ballots)
