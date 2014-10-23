604c7c4d24119cb7675bdd53751a20bbd584daa1
ShotAble <cameroncm@utexas.edu>
2014-10-22 19:17:22 -0500

    renamed functions

--- TestVoting.py
+++ TestVoting.py
@@ -22,7 +22,7 @@ class TestVoting (TestCase) :
 	# Ballot Class
 	# ------------
 
-	def test_ballot_init_1 (self):
+	def test_Ballot_init_1 (self):
 		b = Ballot([1, 2, 3])
 		assert b.votes[b.owner] == 1
 		b.owner += 1
@@ -34,62 +34,69 @@ class TestVoting (TestCase) :
 	# Candidate Class
 	# ---------------
 
-	def test_candidate_init_1 (self): 
+	def test_Candidate_init_1 (self): 
 		pass
 	
-	def test_candidate_give_ballot_1 (self):
+	def test_Candidate_give_ballot_1 (self):
 		pass
 	
-	def test_candidate_take_ballot_1 (self):
+	def test_Candidate_take_ballot_1 (self):
 		pass
 
-	def test_candidate_count_ballots_ 1 (self):
+	def test_Candidate_count_ballots_ 1 (self):
 		pass
 
 	# --------------
 	# Election Class
 	# --------------
 	
-	def test_election_init_1 (self):
+	def test_Election_init_1 (self):
 		pass
 	
-	def test_election_add_candidate_1 (self):
+	def test_Election_add_candidate_1 (self):
 		pass
 
-	def test_election_add_ballot_1 (self):
+	def test_Election_add_ballot_1 (self):
 		pass
 
-	def test_election_look_for_winner_1 (self):
+	def test_Election_look_for_winner_1 (self):
 		pass
 	
-	def test_election_look_for_tie_1 (self):
+	def test_Election_look_for_tie_1 (self):
 		pass
 	
-	def test_election_mark_the_losers_1 (self):
+	def test_Election_mark_the_losers_1 (self):
 		pass
 	
-	def test_election_pass_votes_1 (self):
+	def test_Election_pass_votes_1 (self):
 		pass
 	
 	# ------------------
-	# read_file function
+	# Voting_Read_File function
 	# ------------------
 	
-	def test_read_file_1 (self):
+	def test_Voting_Read_File_1 (self):
 		pass
 	
 	# --------------------
-	# RunElection function
+	# Voting_Run_Election function
 	# --------------------
 	
-	def test_runelection_1 (self):
+	def test_Voting_Run_Election_1 (self):
+		pass
+	
+	# ---------------------
+	# Voting_Write function
+	# ---------------------
+	
+	def test_Voting_Write_1 (self):
 		pass
 	
 	# --------------------
-	# VotingSolve function
+	# Voting_Solve function
 	# --------------------
 	
-	def test_votingsolve_1 (self):
+	def test_Voting_Solve_1 (self):
 		pass
 
 # ----
