0c3d8a1fecd812091a44d5155343a80d2187af31
ShotAble <cameroncm@utexas.edu>
2014-10-23 15:26:58 -0500

    more unit tests for corners

--- TestVoting.py
+++ TestVoting.py
@@ -27,6 +27,16 @@ class TestVoting (TestCase) :
 		assert b.votes[b.owner] == 2
 		b.owner += 1
 		assert b.votes[b.owner] == 3
+	
+	def test_Ballot_init_2 (self):
+		self.assertRaises(TypeError, Ballot, 1, 2, 3)
+	
+	def test_Ballot_init_3 (self):
+		self.assertRaises(TypeError, Ballot, 1)
+	
+	def test_Ballot_init_4 (self):
+		b = Ballot([1])
+		self.assertEqual(b.votes[b.owner], 1)
 
 	# ---------------
 	# Candidate Class
@@ -40,6 +50,11 @@ class TestVoting (TestCase) :
 	
 	def test_Candidate_give_ballot_1 (self):
 		c = Candidate('CL4P-TP')
+		assert not c.ballots
+	
+	def test_Candidate_give_ballot_2 (self):
+		c = Candidate('CL4P-TP')
+		assert not c.ballots
 		b = Ballot ([1, 2, 3])
 		c.give_ballot(b)
 		assert c.ballots
@@ -48,16 +63,32 @@ class TestVoting (TestCase) :
 		c = Candidate('CL4P-TP')
 		b = Ballot ([1, 2, 3])
 		c.give_ballot(b)
+		assert c.ballots
 		c.take_ballot(b)
 		assert not c.ballots
 
 	def test_Candidate_count_ballots_1 (self):
 		c = Candidate('CL4P-TP')
 		b = Ballot ([1, 2, 3])
-		c.give_ballot(b)
-		c.give_ballot(b)
-		c.give_ballot(b)
+		self.assertEqual(c.count_ballots(), 0)
+	
+	def test_Candidate_count_ballots_2 (self):
+		c = Candidate('CL4P-TP')
+		b = Ballot ([1, 2, 3])
+		assert c.count_ballots() == 0
+		for i in range(3):
+			c.give_ballot(b)
+		self.assertEqual(c.count_ballots(), 3)
+	
+	def test_Candidate_count_ballots_3 (self):
+		c = Candidate('CL4P-TP')
+		b = Ballot ([1, 2, 3])
+		assert c.count_ballots() == 0
+		for i in range(3):
+			c.give_ballot(b)
 		assert c.count_ballots() == 3
+		c.take_ballot(b)
+		self.assertEqual(c.count_ballots(), 2)
 
 	# --------------
 	# Election Class
@@ -82,7 +113,14 @@ class TestVoting (TestCase) :
 		e.add_candidate(c)
 		b = Ballot ([1, 2])
 		e.add_ballot(b)
-		assert e.list_ballots
+		self.assertEqual(len(e.list_ballots),1)
+	
+	def test_Election_add_ballot_2 (self):
+		e = Election()
+		e.add_candidate(Candidate('Scooter'))
+		b = Ballot([1, 2])
+		e.add_ballot(b)
+		self.assertEqual(len(e.list_candidates[0].ballots), 1)
 
 	def test_Election_look_for_winner_1 (self): 
 		e = Election()
@@ -109,7 +147,7 @@ class TestVoting (TestCase) :
 		e.add_candidate(Candidate('CL4P-TP'))
 		e.add_candidate(Candidate('Handsome Jack'))
 		for t in range(3):
-			e.add_ballot(Ballot([1]))
+			e.add_ballot(Ballot([1, 2]))
 		assert e.look_for_winner()
 
 	def test_Election_look_for_tie_1 (self): 
@@ -145,6 +183,16 @@ class TestVoting (TestCase) :
 		e.mark_the_losers()
 		assert not e.list_candidates[0].isInRunning
 	
+	def test_Election_mark_the_losers_2 (self):
+		e = Election()
+		for c in ['CL4P-TP', 'Hyperion', 'Handsome Jack']:
+			e.add_candidate(Candidate(c))
+		for b in [[1, 3, 2], [2, 3, 1], [3, 1, 2], [3, 1, 2], [3, 1, 2]]:
+			e.add_ballot(Ballot(b))
+		e.mark_the_losers()
+		assert not e.list_candidates[0].isInRunning
+		assert not e.list_candidates[1].isInRunning
+	
 	def test_Election_pass_votes_1 (self):
 		e = Election()
 		for c in ['CL4P-TP', 'Hyperion', 'Handsome Jack']:
@@ -156,6 +204,18 @@ class TestVoting (TestCase) :
 		assert e.list_candidates[0].count_ballots() == 0
 		assert e.list_candidates[1].count_ballots() == 2
 		assert e.list_candidates[2].count_ballots() == 3
+	
+	def test_Election_pass_votes_2 (self):
+		e = Election()
+		for c in ['CL4P-TP', 'Hyperion', 'Handsome Jack']:
+			e.add_candidate(Candidate(c))
+		for b in [[1, 3, 2], [2, 3, 1], [3, 1, 2], [3, 1, 2], [3, 1, 2]]:
+			e.add_ballot(Ballot(b))
+		e.mark_the_losers()
+		e.pass_votes()
+		assert e.list_candidates[0].count_ballots() == 0
+		assert e.list_candidates[1].count_ballots() == 0
+		assert e.list_candidates[2].count_ballots() == 5
 	
 	# -------------------------
 	# Voting_Read_File function
@@ -210,9 +270,22 @@ class TestVoting (TestCase) :
 		w = StringIO()
 		Voting_Solve(r,w)
 		self.assertEqual(w.getvalue(),'CL4P-TP\n\nKRIEG!\n\n')
+	
+	def test_Voting_Solve_3 (self):
+		r = StringIO('1\n\n4\nAthena\nWilhelm\nNisha\nClaptrap\n')
+		w = StringIO()
+		Voting_Solve(r,w)
+		self.assertEqual(w.getvalue(),'Athena\nWilhelm\nNisha\nClaptrap\n\n')
 
 # ----
 # main
 # ----
 
 main()
+
+'''
+16 functions tested
+30 total tests
+need 48 tests total
+need 18 more tests
+'''
